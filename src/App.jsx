0920cbc30e1449bd40dde3918e95d79c589b962b
jamong13 <limehs0618@gmail.com>
2025-12-03 12:46:03 +0900

    header-feat-01

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,11 @@
 import React from "react";
+import Header from "./components/Header";
 import "./App.css";
 
 export default function App() {
-  return <div></div>;
+  return (
+    <div>
+      <Header />
+    </div>
+  );
 }
