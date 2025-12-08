77bdd2919de24f2fe522c7decc816438547b7689
unknown <rhsowll@gmail.com>
2025-12-08 10:58:07 +0900

    ToEat-docs-02

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,19 @@
-import React from "react";
+import Home from "./homepage/Home";
 import "./App.css";
-import ToEat from "./components/ToEat.jsx";
 
 export default function App() {
-  return <ToEat />;
+  return (
+    <div>
+      <Header />
+      <main className="main-content">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/todo" element={<ToDo />} />
+          <Route path="/towear" element={<ToWear />} />
+          <Route path="/toeat" element={<ToEat />} />
+          <Route path="/towatch" element={<ToWatch />} />
+        </Routes>
+      </main>
+    </div>
+  );
 }
